38eb90024a29f13586446d6a34c093264b9fa994
Haider <Haiderkg78@gmail.com>
2026-09-14 15:32:34 +0530

    type conversion and casting

--- basics.java
+++ basics.java
@@ -11,6 +11,19 @@ class Hello {
         char ch = 'A';
         boolean bl = true;
         int sum = num1 + num2;
+
+        // Widening conversion: smaller type to larger type
+        int wholeNumber = 100;
+        double widenedNumber = wholeNumber;
+
+        // Narrowing conversion: larger type to smaller type using casting
+        double decimalNumber = 99.99;
+        int narrowedNumber = (int) decimalNumber;
+
+        // Casting can also convert between numeric types explicitly
+        int largeValue = 130;
+        byte castByte = (byte) largeValue;
+
         System.out.println("Sum of two numbers: " + sum);
         System.out.println("Byte value: " + by);
         System.out.println("Short value: " + sh);
@@ -19,5 +32,8 @@ class Hello {
         System.out.println("Double value: " + db);
         System.out.println("Character value: " + ch);
         System.out.println("Boolean value: " + bl);
+        System.out.println("Widened int to double: " + widenedNumber);
+        System.out.println("Narrowed double to int: " + narrowedNumber);
+        System.out.println("Cast int to byte: " + castByte);
     }
 }
