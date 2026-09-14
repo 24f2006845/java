04937207934c9f9b9145fa92baa03f49fe3cc044
Haider <Haiderkg78@gmail.com>
2026-09-14 15:22:25 +0530

    java Basics

---
+++ basics.java
@@ -0,0 +1,23 @@
+class Hello {
+    public static void main(String a[]) 
+    {
+        int num1 = 10;
+        int num2 = 20;
+        byte by = 30;
+        short sh = 40;
+        long lg = 50;
+        float fl = 60.0f;
+        double db = 70.0;
+        char ch = 'A';
+        boolean bl = true;
+        int sum = num1 + num2;
+        System.out.println("Sum of two numbers: " + sum);
+        System.out.println("Byte value: " + by);
+        System.out.println("Short value: " + sh);
+        System.out.println("Long value: " + lg);
+        System.out.println("Float value: " + fl);
+        System.out.println("Double value: " + db);
+        System.out.println("Character value: " + ch);
+        System.out.println("Boolean value: " + bl);
+    }
+}
