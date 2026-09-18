609d8fac29470604d0572b4e114204e6df580d71
Haider <Haiderkg78@gmail.com>
2026-09-18 12:51:29 +0530

    Add basics, loops, and student result manager examples in new Java files

--- Basics/javaProgram.java
+++ Basics/javaProgram.java
@@ -1,3 +1,4 @@
+package Basics;
 public class javaProgram {
 	public static void main(String[] args) {
 		java.util.Scanner scanner = new java.util.Scanner(System.in);
--- Basics/loops.java
+++ Basics/loops.java
@@ -1,3 +1,4 @@
+package Basics;
 public class loops {
 	public static void main(String[] args) {
 		// A for loop is useful when the number of repetitions is known.
